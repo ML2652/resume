88e55f525870d3b2db653106194ce3605e072b7d
ML2652 <maricarmenj@yahoo.com>
2017-06-07 07:03:32 -0700

    first commit

---
+++ js/resumeBuilder.js
@@ -0,0 +1,15 @@
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
+// $("#main").append("Maricarmen Lafosse");
+
+var firstName = 'Maricarmen'
+var age = 32;
+console.log (HTMLheaderName);
+
+var formatted_header_name = HTMLheaderName.replace("%data%", firstName);
+console.log(formatted_header_name);
+$("#header").append(formatted_header_name);
+
+
+
