068ad4092d80550748f33613a9cb848570c3a02a
ML2652 <maricarmenj@yahoo.com>
2017-06-13 21:22:50 -0700

    Create resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,15 +1,2 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
-// $("#main").append("Maricarmen Lafosse");
-
-var firstName = 'Maricarmen'
-var age = 32;
-console.log (HTMLheaderName);
-
-var formatted_header_name = HTMLheaderName.replace("%data%", firstName);
-console.log(formatted_header_name);
-$("#header").append(formatted_header_name);
-
-
+    at Object.<anonymous> (C:\Users\RODOLFO\Desktop\frontend-nanodegree-resume-master\js\resumeBuilder.js:11:36)
 
